af5285098fc746ab6f35183f3bf3199adc855741
UmitUmit <umit.green@gmail.com>
2023-06-07 22:44:57 -0500

    @ParameterizedTest     /*@NullSource     @EmptySource//does not work with premitive types*/     @NullAndEmptySource     @ValueSource

--- src/test/java/junit5tests/FirstTestclass.java
+++ src/test/java/junit5tests/FirstTestclass.java
@@ -1,9 +1,29 @@
 package junit5tests;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)//In order to use @BeforeAll and @AfterAll on non-static methods
 public class FirstTestclass {
+
+    @BeforeAll
+    void beforeAll(){
+        System.out.println("--This is the before all method");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("----This is the before each method");
+    }
+
+    @AfterAll
+    void afterAll(){
+        System.out.println("--This is the after all method");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("----This is the after each method");
+    }
 
     @Test
      void firstMethod(){
